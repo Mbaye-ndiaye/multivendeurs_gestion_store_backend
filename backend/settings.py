ce4c660b91af6de627c0cbe0e0072c32562d66d0
Mbaye Babacar Ndiaye <babacarndiay546@gmail.com>
2026-02-23 06:58:39 +0100

    configure l'envoie email

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 from dotenv import load_dotenv, dotenv_values
 import os
 import datetime
@@ -163,8 +162,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'noreply.babacarndiay546@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 20
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
