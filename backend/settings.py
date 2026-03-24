f424321fd13b79ae5b4df850d7ae50774a1f8494
Mbaye Babacar Ndiaye <babacarndiay546@gmail.com>
2026-03-24 15:21:15 +0100

    creer un fichier Procfilesss

--- backend/settings.py
+++ backend/settings.py
@@ -43,11 +43,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'gestionstock.up.railway.app']
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://gestionstock.up.railway.app"
-]
 # Application definition
 
 
