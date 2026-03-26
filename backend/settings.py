2e7ece1aee08468d180ca987d7d2f9eef789a7f0
Mbaye Babacar Ndiaye <babacarndiay546@gmail.com>
2026-03-26 17:48:30 +0100

    ajouter noreply dans setting

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://multivendeurs-gestion-store-backend.onrender.com", "*"]
+ALLOWED_HOSTS = ["multivendeurs-gestion-store-backend.onrender.com", "*"]
 
 # Application definition
 
