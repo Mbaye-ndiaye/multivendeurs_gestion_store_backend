fe04d21dffb6e67895cf882859a6176966e48d77
Mbaye Babacar Ndiaye <babacarndiay546@gmail.com>
2026-03-31 16:44:02 +0100

    dockeriser le projet et fixer le bug deploimenet

--- backend/settings.py
+++ backend/settings.py
@@ -25,10 +25,10 @@ env = dotenv_values(".env")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ["multivendeurs-gestion-store-backend.onrender.com", "*"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["multivendeurs-gestion-store-backend.onrender.com", "*"]
+# ALLOWED_HOSTS = ["*"]
 
 
 
@@ -99,16 +99,16 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
 
 # Custom Simple JWT settings (optionnel)
 SIMPLE_JWT = {
@@ -120,13 +120,13 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
